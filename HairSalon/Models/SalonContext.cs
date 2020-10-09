2d1c7ec6d2ae6f856cf544b4d6f2aabf2715c8e5
Janet Karpenske <janetkarpenske@gmail.com>
2020-10-09 08:32:29 -0700

    Changed name from Salon to HairSalon

--- HairSalon/Models/SalonContext.cs
+++ HairSalon/Models/SalonContext.cs
@@ -1,11 +1,11 @@
 
 using Microsoft.EntityFrameworkCore;
 
-namespace Salon.Models
+namespace HairSalon.Models
 {
   public class SalonContext : DbContext
   {
-    public DbSet<Item> Items { get; set; }
+    public DbSet<Client> Clients { get; set; }
 
     public SalonContext(DbContextOptions options) : base(options) { }
   }
